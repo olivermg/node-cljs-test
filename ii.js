b2d2a3d1151c27178fab994cdfb2357df1f02649
Oliver Wegner <void1976@gmail.com>
2018-01-22 11:46:54 +0100

    call protocol function from javascript

--- ii.js
+++ ii.js
@@ -2,3 +2,7 @@ var lib = require('nc');
 
 console.log('lib:', lib);
 console.log('nc.core.libfn:', lib.nc.core.libfn(123));
+
+var pr1 = lib.nc.core.new_protorec1(5);
+console.log('pr1:', pr1);
+console.log('pr1.protofn:', lib.nc.core.protofn(pr1, 7));
--- src/nc/core.cljs
+++ src/nc/core.cljs
@@ -14,6 +14,19 @@
   (inc (.jj jj x)))
 
 
+(defprotocol Proto1
+  (^:export protofn [this x]))
+
+(defrecord ProtoRec1 [y]
+  Proto1
+  (protofn [this x]
+    (println "Hello from Proto1/protofn function, implemented in ProtoRec1 (local cljs)")
+    (+ x y)))
+
+(defn ^:export new-protorec1 [y]
+  (->ProtoRec1 y))
+
+
 
 ;;; for run-cljs Makefile target:
 
